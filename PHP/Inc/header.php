1f1e855c217a1735ab04ee5a1f181490f6fe4587
Fredius Tout Court <frediustcDev@gmail.com>
2017-12-17 09:23:14 +0000

    Link Done

--- PHP/Inc/header.php
+++ PHP/Inc/header.php
@@ -1,32 +1,6 @@
 <header class="navbar navbar-expand-lg bg-white fixed-top py-0" id="header-top">
   <a class="navbar-brand" href="#" id="logo-header">B}elliseStyle</a>
   <div class="collapse navbar-collapse " id="navbarNav">
-    <ul class="navbar-nav w-100 justify-content-end align-items-center">
-      <li class="nav-item mr-4" data-toggle="tooltip" data-placement="bottom" title="Clients">
-        <a href="#" class="nav-link"><span class="flaticon-transport"></span><span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item mr-4" data-toggle="tooltip" data-placement="bottom" title="Employees">
-        <a href="#" class="nav-link"><span class="flaticon-support"></span></a>
-      </li>
-      <li class="nav-item mr-4" data-toggle="tooltip" data-placement="bottom" title="Produits">
-        <a href="#" class="nav-link active"><span class="flaticon-box-3"></span></a>
-      </li>
-      <li class="nav-item mr-4" data-toggle="tooltip" data-placement="bottom" title="Commandes">
-        <a href="#" class="nav-link"><span class="flaticon-receipt"></span></a>
-      </li>
-      <li class="nav-item mr-4" data-toggle="tooltip" data-placement="bottom" title="Ventes">
-        <a href="#" class="nav-link"><span class="flaticon-banknote"></span></a>
-      </li>
-
-      <li class="nav-item mr-4" data-toggle="tooltip" data-placement="bottom" title="Compte">
-        <a href="#" class="nav-link"><span class="flaticon-options"></span></a>
-      </li>
-      <li class="nav-item mr-4" data-toggle="tooltip" data-placement="bottom" title="Recherche">
-        <a href="#" class="nav-link"><span class="flaticon-magnifying-glass-1"></span></a>
-      </li>
-      <li class="nav-item">
-        <a href="#" class="nav-link" data-toggle="tooltip" data-placement="left" title="Deconnexion"><span class="flaticon-logout-1"></span></a>
-      </li>
-    </ul>
+    <?php include $_ind .'PHP/Inc/menu.php'; ?>
   </div>
 </header>
---
+++ PHP/Inc/menu.php
@@ -0,0 +1,26 @@
+<ul class="navbar-nav w-100 justify-content-end align-items-center">
+  <li class="nav-item mr-4" data-toggle="tooltip" data-placement="bottom" title="Clients">
+    <a href="<?php echo $_ind ?>Customers" class="nav-link"><span class="flaticon-transport"></span><span class="sr-only">(current)</span></a>
+  </li>
+  <li class="nav-item mr-4" data-toggle="tooltip" data-placement="bottom" title="Employees">
+    <a href="<?php echo $_ind ?>Employees" class="nav-link"><span class="flaticon-support"></span></a>
+  </li>
+  <li class="nav-item mr-4" data-toggle="tooltip" data-placement="bottom" title="Produits">
+    <a href="<?php echo $_ind ?>Products" class="nav-link active"><span class="flaticon-box-3"></span></a>
+  </li>
+  <li class="nav-item mr-4" data-toggle="tooltip" data-placement="bottom" title="Commandes">
+    <a href="<?php echo $_ind ?>Orders" class="nav-link"><span class="flaticon-receipt"></span></a>
+  </li>
+  <li class="nav-item mr-4" data-toggle="tooltip" data-placement="bottom" title="Ventes">
+    <a href="<?php echo $_ind ?>Sales" class="nav-link"><span class="flaticon-banknote"></span></a>
+  </li>
+  <li class="nav-item mr-4" data-toggle="tooltip" data-placement="bottom" title="Compte">
+    <a href="<?php echo $_ind ?>Account" class="nav-link"><span class="flaticon-options"></span></a>
+  </li>
+  <li class="nav-item mr-4" data-toggle="tooltip" data-placement="bottom" title="Recherche">
+    <a href="#" class="nav-link"><span class="flaticon-magnifying-glass-1"></span></a>
+  </li>
+  <li class="nav-item">
+    <a href="<?php echo $_ind ?>Logout" class="nav-link" data-toggle="tooltip" data-placement="left" title="Deconnexion"><span class="flaticon-logout-1"></span></a>
+  </li>
+</ul>
